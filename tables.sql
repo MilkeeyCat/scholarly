4d442d890e978a3bb85d2a0c1a38490b68680072
MilkeeyCat <milkeeycat@gmail.com>
2024-03-24 20:07:58 +0200

    refactor: new schema

--- tables.sql
+++ tables.sql
@@ -1,9 +1,14 @@
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(64)
+);
+
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(128),
     last_name VARCHAR(128),
-    year SMALLINT,
-    _group VARCHAR(4)
+    group_id INT REFERENCES groups(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE subjects (
@@ -14,11 +19,14 @@ CREATE TABLE subjects (
 CREATE TABLE teachers (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(128),
-    last_name VARCHAR(128)
+    last_name VARCHAR(128),
+    subject_id INT REFERENCES subjects(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE estimates (
     id SERIAL PRIMARY KEY,
     student_id INT REFERENCES students(id) ON DELETE CASCADE,
-    subject_id INT REFERENCES subjects(id) ON DELETE CASCADE
+    teacher_id INT REFERENCES teachers(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ DEFAULT NOW()
 );
